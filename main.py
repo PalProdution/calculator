9a6a2619d59b28878bf4879b91758065bd8efaa5
Pal patel <115624654+PalProdution@users.noreply.github.com>
2024-08-01 17:17:54 +0530

    Update and rename .gitignore to main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+
+from tkinter import *
+
+def button_press(num):
+
+    global equation_text
+
+    equation_text = equation_text + str(num)
+
+    equation_label.set(equation_text)
+
+def equals():
+
+    global equation_text
+
+    try:
+
+        total = str(eval(equation_text))
+
+        equation_label.set(total)
+
+        equation_text = total
+
+    except SyntaxError:
+
+        equation_label.set("syntax error")
+
+        equation_text = ""
+
+    except ZeroDivisionError:
+
+        equation_label.set("arithmetic error")
+
+        equation_text = ""
