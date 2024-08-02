737886efa525f6bc4baf42b10ffaa0baf87a3f16
Pal patel <115624654+PalProdution@users.noreply.github.com>
2024-08-02 14:30:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,50 @@ def equals():
         equation_label.set("arithmetic error")
 
         equation_text = ""
+
+def clear():
+
+    global equation_text
+
+    equation_label.set("")
+
+    equation_text = ""
+
+
+window = Tk()
+window.title("Calculator program")
+window.geometry("500x500")
+
+equation_text = ""
+
+equation_label = StringVar()
+
+label = Label(window, textvariable=equation_label, font=('consolas',20), bg="white", width=24, height=2)
+label.pack()
+
+frame = Frame(window)
+frame.pack()
+
+button1 = Button(frame, text=1, height=4, width=9, font=35,
+                 command=lambda: button_press(1))
+button1.grid(row=0, column=0)
+
+button2 = Button(frame, text=2, height=4, width=9, font=35,
+                 command=lambda: button_press(2))
+button2.grid(row=0, column=1)
+
+button3 = Button(frame, text=3, height=4, width=9, font=35,
+                 command=lambda: button_press(3))
+button3.grid(row=0, column=2)
+
+button4 = Button(frame, text=4, height=4, width=9, font=35,
+                 command=lambda: button_press(4))
+button4.grid(row=1, column=0)
+
+button5 = Button(frame, text=5, height=4, width=9, font=35,
+                 command=lambda: button_press(5))
+button5.grid(row=1, column=1)
+
+button6 = Button(frame, text=6, height=4, width=9, font=35,
+                 command=lambda: button_press(6))
+button6.grid(row=1, column=2)
