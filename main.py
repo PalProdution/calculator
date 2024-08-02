afc716f246242e1df242acc917b1fda54c243ba5
Pal patel <115624654+PalProdution@users.noreply.github.com>
2024-08-02 18:41:49 +0530

    Last Update main.py

--- main.py
+++ main.py
@@ -79,3 +79,49 @@ button5.grid(row=1, column=1)
 button6 = Button(frame, text=6, height=4, width=9, font=35,
                  command=lambda: button_press(6))
 button6.grid(row=1, column=2)
+
+button7 = Button(frame, text=7, height=4, width=9, font=35,
+                 command=lambda: button_press(7))
+button7.grid(row=2, column=0)
+
+button8 = Button(frame, text=8, height=4, width=9, font=35,
+                 command=lambda: button_press(8))
+button8.grid(row=2, column=1)
+
+button9 = Button(frame, text=9, height=4, width=9, font=35,
+                 command=lambda: button_press(9))
+button9.grid(row=2, column=2)
+
+button0 = Button(frame, text=0, height=4, width=9, font=35,
+                 command=lambda: button_press(0))
+button0.grid(row=3, column=0)
+
+plus = Button(frame, text='+', height=4, width=9, font=35,
+                 command=lambda: button_press('+'))
+plus.grid(row=0, column=3)
+
+minus = Button(frame, text='-', height=4, width=9, font=35,
+                 command=lambda: button_press('-'))
+minus.grid(row=1, column=3)
+
+multiply = Button(frame, text='*', height=4, width=9, font=35,
+                 command=lambda: button_press('*'))
+multiply.grid(row=2, column=3)
+
+divide = Button(frame, text='/', height=4, width=9, font=35,
+                 command=lambda: button_press('/'))
+divide.grid(row=3, column=3)
+
+equal = Button(frame, text='=', height=4, width=9, font=35,
+                 command=equals)
+equal.grid(row=3, column=2)
+
+decimal = Button(frame, text='.', height=4, width=9, font=35,
+                 command=lambda: button_press('.'))
+decimal.grid(row=3, column=1)
+
+clear = Button(window, text='clear', height=4, width=12, font=35,
+                 command=clear)
+clear.pack()
+
+window.mainloop()
